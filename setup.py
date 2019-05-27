0a8aef6c69706ce8a0e2efb7fdac0b24e7e7167c
aanant <aanant@umich.edu>
2019-05-26 22:19:49 -0400

    basic location code

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 import os
 
-template_dir = os.path.abspath('./public_html')
+template_dir = os.path.abspath('./templates')
 
 app = Flask(__name__, template_folder=template_dir)
 
 @app.route("/")
 def hello():
     return render_template('location.html')
+
+

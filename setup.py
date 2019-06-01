227518c21a1a7a75f7bc0c26979fcd2cbd30184e
aanant <aanant@umich.edu>
2019-06-01 14:05:18 -0400

    set up classrooms db

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ db = SQLAlchemy(app)
 
 ### Test Users table for checking db connection
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     
@@ -23,19 +23,92 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-admin = User('admin', 'admin@example.com')
+class Classes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    className = db.Column(db.String(80), unique=True, nullable=False)
+    teacherId = db.Column(db.Integer)
+    numStudents = db.Column(db.Integer)
 
-db.session.add(admin)
+    def __init__(self, className, teacherId, numStudents):
+        self.className = className
+        self.teacherId = teacherId
+        self.numStudents = numStudents
 
-print(admin)
-db.session.commit() # This is needed to write the changes to database
+    def __repr__(self):
+        return "Class {} {}".format(self.className, self.teacherId)
 
-print(User.query.all())
 
-print(User.query.filter_by(username='admin').first())
 
-@app.route("/")
-def hello():
+class Classrooms(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    classroomName = db.Column(db.String(80), unique=True)
+    lat = db.Column(db.Float)
+    lng = db.Column(db.Float)
+
+    def __init__(self, classroomName, lat, lng):
+        self.classroomName = classroomName
+        self.lat = lat
+        self.lng = lng
+
+    def __repr__(self):
+        return "{} {},{}".format(self.classroomName, self.lat, self.lng)
+
+#db.create_all()
+
+#classroom = Classrooms('1003 EECS', 42.292756,-83.714665)
+#db.session.add(classroom)
+#db.session.commit()
+
+print(Classrooms.query.all())
+# admin = User('admin', 'admin@example.com')
+
+# db.session.add(admin)
+
+# print(admin)
+# db.session.commit() # This is needed to write the changes to database
+
+# print(User.query.all())
+
+# print(User.query.filter_by(username='admin').first())
+
+
+## TODO - Import coordinates for classrooms
+## Create student signup process
+# Create API endpoints for 1. creating new class and table 2. student signup 3. Exporting attendance table to csv and emailing it
+# Create nice looking form input
+# Create database of EECS professors
+
+@app.route("/class", methods=["GET", "POST"])
+def add_class():
+    if request.method == "POST":
+        newClass = Classes('eecs442','1','0')
+        db.session.add(newClass)
+        print(newClass)
+        db.session.commit()
+        return render_template('location.html')
+    else:
+        teacherId = request.args.get("teacherId")
+        print("Teacher Id {}".format(teacherId))
+        
+
+@app.route("/", methods=["GET", "POST"])
+def firstPage():
     return render_template('location.html')
+
+@app.route("/<className>", methods = ["GET","POST"])
+def index(className):
+    print(className)
+    return render_template('location.html')
+
+def testing():
+    if request.method == "POST":
+        print("post request worked")
+    return render_template('location.html')
+
+
+     
+
+
+
 
 

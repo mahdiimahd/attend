408559ad8ccfa7f95d554844aebc2cf5a727c61d
aanant <aanant@umich.edu>
2019-06-01 17:38:55 -0400

    added static folder

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template
 import os
 
 template_dir = os.path.abspath('./templates')
+static_dir = os.path.abspath('./templates/assets')
 
-app = Flask(__name__, template_folder=template_dir)
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir, static_url_path='')
 
 @app.route("/")
 def hello():

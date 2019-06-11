1f07a4aa6ca210a14f8d192268610b8e2a7f4189
aanant <aanant@umich.edu>
2019-06-10 21:01:40 -0400

    more api endpoints

--- app.py
+++ app.py
@@ -34,13 +34,33 @@ class Classes(db.Model):
     teacherUniqname = db.Column(db.String(80))
     numStudents = db.Column(db.Integer)
     classroomName = db.Column(db.String(80))
+    mondayClass = db.Column(db.String(80))
+    tuesdayClass = db.Column(db.String(80))
+    wednesdayClass = db.Column(db.String(80))
+    thursdayClass = db.Column(db.String(80))
+    fridayClass = db.Column(db.String(80))
+    mondayTime = db.Column(db.Integer)
+    tuesdayTime = db.Column(db.Integer)
+    wednesdayTime = db.Column(db.Integer)
+    thursdayTime = db.Column(db.Integer)
+    fridayTime = db.Column(db.Integer)
 
-    def __init__(self, className, teacherName, teacherUniqname, classroomName):
+    def __init__(self, className, teacherName, teacherUniqname, classroomName, mondayClass, mondayTime, tuesdayClass, tuesdayTime, wednesdayClass, wednesdayTime, thursdayClass, thursdayTime, fridayClass, fridayTime):
         self.className = className.replace(" ","").lower()
         self.classroomName = classroomName
         self.teacherName = teacherName
         self.teacherUniqname = teacherUniqname
         self.numStudents = 0
+        self.mondayClass = mondayClass
+        self.mondayTime = mondayTime
+        self.tuesdayClass = tuesdayClass
+        self.tuesdayTime = tuesdayTime
+        self.wednesdayClass = wednesdayClass
+        self.wednesdayTime = wednesdayTime
+        self.thursdayClass = thursdayClass
+        self.thursdayTime = thursdayTime
+        self.fridayClass = fridayClass
+        self.fridayTime = fridayTime
 
 
     def __repr__(self):
@@ -69,8 +89,8 @@ class Singleclass(db.Model):
 class Classrooms(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     classroomName = db.Column(db.String(80), unique=True)
-    lat = db.Column(db.Float)
-    lng = db.Column(db.Float)
+    lat = db.Column(db.Numeric(2,7))
+    lng = db.Column(db.Numeric(2,7))
 
     def __init__(self, classroomName, lat, lng):
         self.classroomName = classroomName
@@ -81,7 +101,7 @@ class Classrooms(db.Model):
         return "{} {},{}".format(self.classroomName, self.lat, self.lng)
 
 db.create_all()
-
+db.session.commit()
 #classroom = Classrooms('1003 EECS', 42.292756,-83.714665)
 #db.session.add(classroom)
 #db.session.commit()
@@ -125,13 +145,27 @@ def firstPage():
 @app.route("/addClass", methods=["POST"])
 def addClass():
     if request.method == 'POST':
-        newClass = Classes(request.form["className"], request.form["teacherName"], request.form["teacherUniqname"], request.form["classroomName"])
+        newClass = Classes(
+            className=request.form["className"],
+            teacherName=request.form["teacherName"],
+            teacherUniqname=request.form["teacherUniqname"],
+            classroomName=request.form["classroomName"],
+            mondayClass=request.form["mondayClass"],
+            mondayTime=request.form["mondayTime"],
+            tuesdayClass=request.form["tuesdayClass"],
+            tuesdayTime=request.form["tuesdayTime"],
+            wednesdayClass=request.form["wednesdayClass"],
+            wednesdayTime=request.form["wednesdayTime"],
+            thursdayClass=request.form["thursdayClass"],
+            thursdayTime=request.form["thursdayTime"],
+            fridayClass=request.form["fridayClass"],
+            fridayTime=request.form["fridayTime"])
         db.session.add(newClass)
         db.session.commit()
         print(newClass)
         return jsonify(
             message="New class created",
-            url="https://www.attendapp.me/" + newClass.className
+            url="https://www.attendapp.me/c/" + newClass.className
         )
 
 
@@ -155,6 +189,17 @@ def getAllClasses():
             classes=class_list_response
         )
 
+@app.route("/getClass/<className>", methods=["GET"])
+def getClassInfo(className):
+    if request.method == 'GET':
+        class_info = Classes.query.filter_by(className=className).first()
+        classroom_info = Classrooms.query.filter_by(classroomName=class_info.classroomName).first()
+        print(class_info,classroom_info)
+
+        return jsonify(
+            classInfo=class_info,
+            classroomInfo=classroom_info
+        )
 
 
 @app.route("/c/<className>", methods = ["GET","POST"])
@@ -162,9 +207,49 @@ def index(className):
     if request.method == 'POST':
         print("post request received")
         # print(request.data.keys())
-        print(datetime.now())
+        classInfo = Classes.query.filter_by(className=className).first()
+        print(classInfo)
+        date = datetime.now()
+        day = date.weekday()
+        is_day_valid = ""
+        is_time_valid = ""
+        if day == 0:
+            is_day_valid = classInfo.mondayClass
+            is_time_valid = classInfo.mondayTime
+        elif day == 1:
+            is_day_valid  = classInfo.tuesdayClass
+            is_time_valid = classInfo.tuesdayTime
+        elif day == 2:
+            is_day_valid = classInfo.wednesdayClass
+            is_time_valid = classInfo.wednesdayTime
+        elif day == 3:
+            is_day_valid = classInfo.thursdayClass
+            is_time_valid = classInfo.thursdayTime
+        elif day == 4:
+            is_day_valid = classInfo.fridayClass
+            is_time_valid = classInfo.fridayTime
+        else:
+            is_day_valid = "False"
+            is_time_valid = -1
+        print(day, is_day_valid, is_time_valid, date.hour)
+        if is_day_valid == "False":
+            return jsonify(
+                message="Class is not happening today"
+            )
+        if is_time_valid != date.hour:
+            return jsonify(
+                message="Class is not in session right now"
+            )
+
+        last_record = Singleclass.query.filter_by(uniqname=request.form['uniqname']).order_by(Singleclass.date.asc()).all()
+        if len(last_record) > 0:
+            last_record = last_record[-1]
+            if last_record.date.date() == date.date():
+                return jsonify(
+                    message="You have already checked in today."
+                )
         entry = Singleclass(request.form['studentName'], className, request.form['uniqname'])
-        print(entry)
+        #print(entry)
         db.session.add(entry)
         db.session.commit()
         return "{ 'Status' : 'OK'}"
@@ -180,6 +265,25 @@ def getAttendance(classname):
         print(num_students)
         return jsonify(
             count=len(num_students),
+        )
+
+@app.route("/classroom", methods=["GET","POST"])
+def addClassroom():
+    if request.method == 'POST':
+        classroom = Classrooms(request.form["roomNumber"] + " " + request.form["buildingName"], request.form["lat"] , request.form["lng"])
+        db.session.add(classroom)
+        db.session.commit()
+        return jsonify(
+            message="Building added"
+        )
+    else:
+        list_classrooms = Classrooms.query.all()
+        classrooms = {}
+        for c in list_classrooms:
+            classrooms[str(c.classroomName)] = str(c.lat) + "," + str(c.lng)
+        print (classrooms)
+        return jsonify(
+            classrooms
         )
 
 # def testing():
